659dead82efde2b3ae2aa08339753d041161dc3f
Aline Coelho <alinecoelho1214@hotmail.com>
2024-02-18 16:28:31 -0300

    Add files via upload

---
+++ condição simples.js
@@ -0,0 +1,8 @@
+var vel = 60.5 
+console.log (`A velocidade do seu carro é ${vel} Km\hr`)
+
+if (vel > 60 ){//condição simples
+    console.log (`Você ultrapassou a velocidade perimitida. MULTADO! `)
+}
+
+console.log (`Dirija sempre usando o cinto de segurança`)
